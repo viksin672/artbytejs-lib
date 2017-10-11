c0c764e9dd4de540074c60fa1f23d1c6a70c4e68
Bushstar <bushsolo@gmail.com>
2017-10-11 04:18:46 -0400

    Add ArtByte

--- src/networks.js
+++ src/networks.js
@@ -33,5 +33,15 @@ module.exports = {
     pubKeyHash: 0x30,
     scriptHash: 0x32,
     wif: 0xb0
+  },
+  artbyte: {
+    messagePrefix: '\x19ArtByte Signed Message:\n',
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe
+    },
+    pubKeyHash: 0x17,
+    scriptHash: 0x05,
+    wif: 0x97
   }
 }
